7a67d61bdea0d17a4cd9868c6f91dcb788e0b2a8
santk123 <santcomar@gmail.com>
2025-06-02 22:01:21 +0530

    dfasdfsdfsadf

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import React from "react";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+// import React from 'react';
+
+const Navbar = () => {
+  return <h1>kumar kuddana</h1>;
+};
+
+export default Navbar;
