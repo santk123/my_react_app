22ba8f6c1c954a6a6a3b16ae9cbf715fb0b718ec
santk123 <santcomar@gmail.com>
2025-06-05 15:31:06 +0530

    dsfdsfdsfds

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 // import React from 'react';
 
 const Navbar = () => {
-  return <h1>This is my first react santosh on github</h1>;
+  return <h1>This is my first react kumar kuddana on github</h1>;
 };
 export default Navbar;
